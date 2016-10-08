89a317b3d35065d2cb66755c422f738ae7545503
border-radius <pharshmaster@gmail.com>
2016-10-08 17:49:45 +0300

    posting

--- index.js
+++ index.js
@@ -1,35 +1,84 @@
 'use strict'
 
 var Promise = require('bluebird')
+var request = require('request')
 var bot = require('./lib/bot')
 var user = require('./lib/user')
+var message = require('./lib/message')
 var ws = require('./lib/ws')
 
-ws('wss://bnw.im/ws?v=2', json => {
-  try {
-    var message = JSON.parse(json)
-  } catch (e) {
-    console.error(e)
-    return
+ws('wss://bnw.im/ws?v=2', event => {
+  switch (event.type) {
+    case 'new_message':
+      var post = [
+        '@' + event.user + ':',
+        event.text,
+        'https://6nw.im/p/' + event.id
+      ].join('\n\n')
+
+      user.find({
+        subscribed: { $ne: null }
+      }).then(users => {
+        Promise.each(users, user => {
+          return new Promise((resolve, reject) => {
+            bot.sendMessage(user.id, post).then(resolve).catch(e => {
+              console.error(user.username, user.id, e.message)
+              resolve()
+            })
+          })
+        })
+      })
+      break
+
+    case 'upd_recommendations_count':
+      message.findOne({
+        message: event.id,
+        subscribed: { $ne: null }
+      }).then(subscription => {
+        if (!subscription) {
+          return
+        }
+
+        var newRecommendation = event.recommendations.filter(recommendation => {
+          return subscription.recommendations.indexOf(recommendation) === -1
+        }).pop()
+
+        if (!newRecommendation) {
+          return
+        }
+
+        var post = '@' + newRecommendation + ' recommended your post https://6nw.im/p/' + event.id + '\n>' + subscription.text.slice(0, 60)
+        bot.sendMessage(subscription.userid, post)
+        subscription.recommendations = event.recommendations
+        subscription.save()
+      })
+      break
   }
+})
 
-  if (message.type !== 'new_message') {
-    return
-  }
-
-  var post = [
-    '@' + message.user + ':',
-    message.text,
-    'https://meow.bnw.im/p/' + message.id
-  ].join('\n\n')
-
-  user.find({
+ws('wss://bnw.im/comments/ws', comment => {
+  message.findOne({
+    message: comment.replyto || comment.message,
     subscribed: { $ne: null }
-  }).then(users => {
-    Promise.each(users, user => {
-      return bot.sendMessage(user.id, post)
-      .catch(e => console.error(user.username, user.id, e.message))
-    })
+  }).then(subscription => {
+    if (!subscription) {
+      return
+    }
+
+    if (comment.replyto) {
+      comment.text = comment.text.split(' ').slice(1).join(' ')
+    }
+
+    var post = [
+      '@' + comment.user,
+      '>' + subscription.text.slice(0, 60),
+      '',
+      comment.text,
+      '',
+      'https://6nw.im/p/' + comment.id.replace('/', '#')
+    ].join('\n')
+
+    bot.sendMessage(subscription.userid, post)
   })
 })
 
