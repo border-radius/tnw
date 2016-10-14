190ae892a78167580f8e3107a78d2e96eec7245b
border-radius <pharshmaster@gmail.com>
2016-10-14 15:08:54 +0300

    more tests

--- test/utils.js
+++ test/utils.js
@@ -9,4 +9,28 @@ describe('utils', () => {
     assert.equal(id, 'U00HX6')
     done()
   })
+
+  it('should check correct id', done => {
+    var result = utils.checkId('U00HX6')
+    assert.equal(result, true)
+    done()
+  })
+
+  it('should check incorrect id', done => {
+    var result = utils.checkId('HUYPIZD')
+    assert.equal(result, false)
+    done()
+  })
+
+  it('should check correct full id', done => {
+    var result = utils.checkFullId('U00HX6/3DS')
+    assert.equal(result, true)
+    done()
+  })
+
+  it('should check incorrect full id', done => {
+    var result = utils.checkFullId('HUYPIZD/123')
+    assert.equal(result, false)
+    done()
+  })
 })
